cae091612886e99173aaa83744caeb244bd9986e
Vinicius Alves <139510555+Vitenskapp@users.noreply.github.com>
2025-03-17 19:15:35 -0300

    fix: Authentication endpoint cross origin fixed

--- src/main/java/com/vinicius/web_posting/Controller/AuthenticationController.java
+++ src/main/java/com/vinicius/web_posting/Controller/AuthenticationController.java
@@ -15,13 +15,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
